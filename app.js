7fd8c5270111e198fdd8aca94e88a914108c024e
dylaanGc <dylanhernandez337@gmail.com>
2026-09-20 17:52:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -90,19 +90,8 @@ const products = [
     class: "tee",
     image: "img/tee-mono.jpg.jpeg",
     desc: "Camiseta minimalista Hypefriends."
-     
   }
 
-   {
-    id: 9,
-    name: "SHORT CORTEIZ",
-    price: 35000,
-    stock: 1,
-    category: "short",
-    class: "short",
-    image: "img/short-white.jpg.jpeg",
-    desc: "Demin short corteiz."
-  },
 ];
 
 
