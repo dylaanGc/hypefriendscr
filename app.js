dddd53f0f7cb92833a8e3ef1b2cfe3b8536b1762
dylaanGc <dylanhernandez337@gmail.com>
2026-09-20 22:12:59 -0600

    Update greeting from 'Hello' to 'Goodbye'

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
       stock: 6,
       category: "hoodie",
       class: "hoodie",
-      image: "img/hoodie-grey.jpg.jpeg",
+      image: "img/hoodie-core.jpg.jpeg",
       desc: "Edición Essentials con unidades limitadas."
     },
 
